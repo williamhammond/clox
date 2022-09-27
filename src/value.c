beccb54591e5814ae7949047f8ad3ea22bee1364
William Hammond <william.t.hammond@gmail.com>
2022-09-27 10:53:34 -0400

    Implements NaN boxing

--- src/value.c
+++ src/value.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 
 #include "memory.h"
-#include "value.h"
 #include "object.h"
+#include "value.h"
 
 void initValueArray(ValueArray *array) {
   array->values = NULL;
@@ -43,6 +42,17 @@ void setValue(ValueArray *array, int index, Value value) {
 }
 
 void printValue(Value value) {
+#ifdef NAN_BOXING
+  if (IS_BOOL(value)) {
+    printf(AS_BOOL(value) ? "true" : "false");
+  } else if (IS_NIL(value)) {
+    printf("nil");
+  } else if (IS_NUMBER(value)) {
+    printf("%g", AS_NUMBER(value));
+  } else if (IS_OBJ(value)) {
+    printObject(value);
+  }
+#else
   switch (value.type) {
   case VAL_BOOL:
     printf(AS_BOOL(value) ? "true" : "false");
@@ -57,9 +67,16 @@ void printValue(Value value) {
     printObject(value);
     break;
   }
+#endif
 }
 
 bool valuesEqual(Value a, Value b) {
+#ifdef NAN_BOXING
+  if (IS_NUMBER(a) && IS_NUMBER(b)) {
+    return AS_NUMBER(a) == AS_NUMBER(b);
+  }
+  return a == b;
+#else
   if (a.type != b.type)
     return false;
   switch (a.type) {
@@ -74,4 +91,5 @@ bool valuesEqual(Value a, Value b) {
   default:
     return false; // Unreachable.
   }
+#endif
 }
